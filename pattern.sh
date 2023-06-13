92b8917b4d0bf960571519a1a404a03471e0ca4b
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-13 23:51:21 +0530

    Added :Diamond

--- pattern.sh
+++ pattern.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+# Set the number of rows in the diamond
 rows=3
 
-for ((i=$rows; i>=1; i--))
+for ((i=1; i<=rows+1; i++))
 do
-  for ((j=1; j<=$rows-i; j++))
+  for ((j=1; j<=rows+1-i; j++))
+  do
+    echo -n " "
+  done
+
+  for ((j=1; j<=2*i-1; j++))
+  do
+    echo -n "*"
+  done
+
+  echo ""
+done
+
+for ((i=rows; i>=1; i--))
+do
+
+  for ((j=1; j<=rows+1-i; j++))
   do
     echo -n " "
   done
