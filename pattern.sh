15917a29e9425fadd947dd6b61923ddf9db77a07
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-14 00:07:08 +0530

    Added :Give the Option to the user to print the pattern

--- pattern.sh
+++ pattern.sh
@@ -1,33 +1,130 @@
 #!/bin/bash
 
-rows=3
+echo "Select a pattern to print:"
+echo "1. Left Pyramid"
+echo "2. Right Pyramid"
+echo "3. Pyramid"
+echo "4. Inverted Pyramid"
+echo "5. Diamond"
+echo "6. Hour Glass"
 
-for ((i=1; i<=rows+1; i++))
-do
-  for ((j=1; j<=i-1; j++))
-  do
-    echo -n " "
-  done
+read choice
 
-  for ((j=1; j<=2*(rows+1-i)-1; j++))
-  do
-    echo -n "*"
-  done
+echo "Enter the number of rows:"
+read rows
 
-  echo ""
-done
-
-for ((i=rows; i>=1; i--))
-do
-  for ((j=1; j<=i-1; j++))
-  do
-    echo -n " "
-  done
-
-  for ((j=1; j<=2*(rows+1-i)-1; j++))
-  do
-    echo -n "*"
-  done
-
-  echo ""
-done
+case $choice in
+  1)
+    # Print the left pyramid
+    for ((i=1; i<=rows; i++))
+    do
+      for ((j=1; j<=i; j++))
+      do
+        echo -n "*"
+      done
+      echo ""
+    done
+    ;;
+  2)
+    # Print the right pyramid
+    for ((i=1; i<=rows; i++))
+    do
+      for ((j=1; j<=rows-i; j++))
+      do
+        echo -n " "
+      done
+      for ((j=1; j<=i; j++))
+      do
+        echo -n "*"
+      done
+      echo ""
+    done
+    ;;
+  3)
+    # Print the pyramid
+    for ((i=1; i<=rows; i++))
+    do
+      for ((j=1; j<=rows-i; j++))
+      do
+        echo -n " "
+      done
+      for ((j=1; j<=2*i-1; j++))
+      do
+        echo -n "*"
+      done
+      echo ""
+    done
+    ;;
+  4)
+    # Print the inverted pyramid
+    for ((i=$rows; i>=1; i--))
+    do
+      for ((j=1; j<=$rows-i; j++))
+      do
+        echo -n " "
+      done
+      for ((j=1; j<=2*i-1; j++))
+      do
+        echo -n "*"
+      done
+      echo ""
+    done
+    ;;
+  5)
+    # Print the diamond
+    for ((i=1; i<=rows+1; i++))
+    do
+      for ((j=1; j<=rows+1-i; j++))
+      do
+        echo -n " "
+      done
+      for ((j=1; j<=2*i-1; j++))
+      do
+        echo -n "*"
+      done
+      echo ""
+    done
+    for ((i=rows; i>=1; i--))
+    do
+      for ((j=1; j<=rows+1-i; j++))
+      do
+        echo -n " "
+      done
+      for ((j=1; j<=2*i-1; j++))
+      do
+        echo -n "*"
+      done
+      echo ""
+    done
+    ;;
+  6)
+    # Print the hourglass
+    for ((i=1; i<=rows+1; i++))
+    do
+      for ((j=1; j<=i-1; j++))
+      do
+        echo -n " "
+      done
+      for ((j=1; j<=2*(rows+1-i)-1; j++))
+      do
+        echo -n "*"
+      done
+      echo ""
+    done
+    for ((i=rows; i>=1; i--))
+    do
+      for ((j=1; j<=i-1; j++))
+      do
+        echo -n " "
+      done
+      for ((j=1; j<=2*(rows+1-i)-1; j++))
+      do
+        echo -n "*"
+      done
+      echo ""
+    done
+    ;;
+  *)
+    echo "Invalid choice. Please try again."
+    ;;
+esac
