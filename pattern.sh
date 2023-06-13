d1625ab2c4f40bb57913d3d864df2e24a691db9d
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-13 23:26:30 +0530

    Added :Pyramid

--- pattern.sh
+++ pattern.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+# Set the number of rows in the pyramid
 rows=4
 
+# Loop through each row
 for ((i=1; i<=rows; i++))
 do
+  # Print the spaces for the current row
   for ((j=1; j<=rows-i; j++))
   do
     echo -n " "
   done
 
-  for ((j=1; j<=i; j++))
+  # Print the asterisks for the current row
+  for ((j=1; j<=2*i-1; j++))
   do
     echo -n "*"
   done
 
+  # Move to the next line
   echo ""
 done
