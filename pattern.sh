c36963aa4865544cfa4b45338f302ec019309579
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-13 23:31:34 +0530

    Added :Inverted pyramid

--- pattern.sh
+++ pattern.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-# Set the number of rows in the pyramid
-rows=4
+rows=3
 
-# Loop through each row
-for ((i=1; i<=rows; i++))
+for ((i=$rows; i>=1; i--))
 do
-  # Print the spaces for the current row
-  for ((j=1; j<=rows-i; j++))
+  for ((j=1; j<=$rows-i; j++))
   do
     echo -n " "
   done
 
-  # Print the asterisks for the current row
   for ((j=1; j<=2*i-1; j++))
   do
     echo -n "*"
   done
 
-  # Move to the next line
   echo ""
 done
